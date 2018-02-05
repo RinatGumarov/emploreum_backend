2f3d6becc55baab72e7f7f9a89a61209697ee79d
mikl778890@yandex.ru <adminadmin123>
2018-02-06 01:33:34 +0300

    add comment to spec module

--- src/modules/specialisation/services/profilesService.js
+++ src/modules/specialisation/services/profilesService.js
@@ -4,10 +4,19 @@ console.log(Profiles);
 
 class ProfileService {
 
+    /**
+     * поиск всех профилей
+     * @returns {Promise.<Array.<Model>>}
+     */
     all() {
         return Profiles.findAll();
     }
 
+    /**
+     * поиск по имени
+     * @param name
+     * @returns {Promise.<Model>}
+     */
     findOneByName(name) {
         return Profiles.findOne({
             where: {
