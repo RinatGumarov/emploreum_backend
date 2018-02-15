32ff99cddecc87f0cc8f66f24725a2587f1f07a8
mikl778890@yandex.ru <adminadmin123>
2018-02-15 12:38:40 +0300

    add try catch tag

--- src/modules/message/controllers/chatController.js
+++ src/modules/message/controllers/chatController.js
@@ -6,18 +6,21 @@ module.exports.func = (router) => {
 
     router.get('/chats/all', async (req, res) => {
 
-        let chats;
+        try {
+            let chats;
+            if (req.user.role === "EMPLOYEE") {
+                let employee = await employeeService.getByUserId(req.user.id);
+                chats = await chatService.getAllChatsFroEmployee(employee.id);
+            } else {
+                let company = await companyService.findByUserId(req.user.id);
+                chats = await chatService.getAllChatsFroCompany(company.id);
+            }
+            res.json(chats)
+        } catch (err) {
+            logger.error(err.stack);
+            res.status(500).send({error: 'Could not get all chats'});
 
-        if (req.user.role === "EMPLOYEE") {
-            let employee = await employeeService.getByUserId(req.user.id);
-            chats = await chatService.getAllChatsFroEmployee(employee.id);
-        } else {
-            let company = await companyService.findByUserId(req.user.id);
-            chats = await chatService.getAllChatsFroCompany(company.id);
         }
-
-        res.json(chats)
-
     });
 
     return router;
