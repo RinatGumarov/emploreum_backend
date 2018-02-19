ca260a256f9086efb7b1f074d906c632bfff5cf0
rinat <tiran678@icloud.com>
2018-02-19 17:36:32 +0300

    migr

--- src/migrations/20180219085117-add_has_contract_column_to_employee_and_company.js
+++ src/migrations/20180219085117-add_has_contract_column_to_employee_and_company.js
@@ -4,11 +4,11 @@ module.exports = {
     up(queryInterface, Sequelize) {
         return queryInterface.sequelize.transaction(() => {
             return Promise.all([
-                queryInterface.addColumn("employees", "has_contract", {
+                queryInterface.addColumn("employees", "contract", {
                     type: Sequelize.BOOLEAN,
                     defaultValue: false,
                 }, {}),
-                queryInterface.addColumn("companies", "has_contract", {
+                queryInterface.addColumn("companies", "contract", {
                     type: Sequelize.BOOLEAN,
                     defaultValue: false,
                 }, {}),
