6a7cea6870d28ac8d8a6ebb0b5ed4f86d65bc167
mikl778890@yandex.ru <adminadmin123>
2018-02-19 22:17:56 +0300

    add async blockchain

--- src/modules/employee/controllers/employeeController.js
+++ src/modules/employee/controllers/employeeController.js
@@ -3,6 +3,7 @@ const cvService = require('../services/cvService');
 const employeeService = require('../services/employeeService');
 const companyService = require('../../company/services/companyService');
 const messageService = require('../../message/services/messageService');
+const testService = require('../../blockchain/services/blockChainEventService');
 const vacancyService = require('../../company/services/vacancyService');
 const workService = require('../services/workService');
 const logger = require('../../../utils/logger');
@@ -28,6 +29,7 @@ module.exports.func = (router) => {
             //todo employee user
             workService.createWork(vacancy, employee, company, vacancyId, req.user.account_address);
             await messageService.sendToCompany(req.user.id, company.id, "Вам постучались на вакансию");
+            testService.set(req.user.id, "asd");
             return res.send({data: 'success'});
         }
         catch (err) {
@@ -41,12 +43,12 @@ module.exports.func = (router) => {
         let recommendedVacancies = await vacancyService.getRecommended(employeeSkills, req.user.id);
         res.json(recommendedVacancies);
     });
-
+    
     router.get('/info/:employeeUserId', async (req, res) => {
         let employee = await employeeService.getByUserId(req.params.employeeUserId);
         res.json(employee);
     });
-
+    
     router.get('/skills/:employeeUserId', async (req, res) => {
         let employeeSkills = await cvService.getEmployeeSkillsWithProfiles(req.params.employeeUserId);
         res.json(employeeSkills);
