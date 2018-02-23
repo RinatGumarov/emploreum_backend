d563da67dcd7b029800dac7c4ab1492f3f589e38
rinat <tiran678@icloud.com>
2018-02-23 22:14:06 +0300

    test

--- src/modules/test/controllers/testController.js
+++ src/modules/test/controllers/testController.js
@@ -1,6 +1,29 @@
 module.exports.func = (router) => {
 
-    router.post('')
+    router.post('/create', async (req, res) => {
+        try {
+            let company = await companyService.findByUserId(req.user.id);
+            let options = req.body;
+            let vacancy = await vacancyService.save(options);
+            let profiles = options.specifications;
+            await profiles.forEach(async (profile) => {
+                await profile.skills.forEach(async (skill) => {
+                    let profileSkill = await vacancyService.findProfileSkill(profile.id, skill.id);
+                    let vacancyProfileSkill = await vacancyService.addProfileSkillToVacancy({
+                        vacancy_id: vacancy.id,
+                        profile_skill_id: profileSkill.id
+                    });
+                    logger.log(vacancyProfileSkill);
+                });
+            });
+
+            return res.status(200).send({vacancy: vacancy});
+        }
+        catch (err) {
+            logger.error(err.stack);
+            return res.status(500).send({error: err.message});
+        }
+    });
 
     return router;
 
