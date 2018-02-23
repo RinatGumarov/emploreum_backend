73f1b80e6d9a1ba0b4aeafdf480a5a74de828eea
rinat <tiran678@icloud.com>
2018-02-23 18:03:59 +0300

    test_profile_skills migration

---
+++ src/migrations/20180223144915-add_test_profile_skills_table.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('test_profile_skills', {
+            vacancy_id: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                references: {
+                    model: "tests",
+                    key: "id",
+                },
+                cascade: true,
+                onDelete: "CASCADE",
+            },
+            profile_skill_id: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                references: {
+                    model: "profile_skills",
+                    key: "id",
+                },
+                cascade: true,
+                onDelete: "CASCADE",
+            }
+        });
+    },
+
+    down: (queryInterface) => {
+        return queryInterface.dropTable('test_profile_skills', {});
+    }
+};
