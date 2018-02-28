67c75f5eae0195e3a1eff555611b585f50679770
mikl778890@yandex.ru <adminadmin123>
2018-02-28 23:19:06 +0300

    hot fix

---
+++ src/modules/message/services/notificationService.js
@@ -0,0 +1,62 @@
+const models = require('../../../core/models');
+const Notifications = models.notifications;
+const Op = models.sequelize.Op;
+const socketSender = require('../../../core/socketSender');
+
+
+let instance;
+
+/**
+ * сервис для отправки уведомлений
+ * по сокету
+ */
+class NotificationService {
+    
+    /**
+     * послать уведомление
+     * @param userId
+     * @param text
+     * @returns {Promise<*>}
+     */
+    async sendNotification(userId, text) {
+        let notification = await Notifications.create({
+            user_id: userId,
+            text: text
+        });
+        await socketSender.sendSocketMessage(userId, notification);
+        return notification;
+    };
+    
+    /**
+     * показать новые уведомления
+     * и сделать их прчоитанными
+     */
+    async getAllNotifications(user) {
+        let notifications = await Notifications.findAll({
+            where: {
+                is_view: {[Op.eq]: true},
+                user_id: {[Op.eq]: user.id}
+            },
+            order: [
+                ['is_view', 'DESC'],
+            ]
+        });
+        
+        return notifications;
+    }
+    
+    async readNotifications(user) {
+        Notifications.update({
+            is_view: true
+        }, {
+            where: {
+                is_view: {[Op.eq]: false},
+                user_id: {[Op.eq]: user.id}
+            }
+        });
+    }
+    
+}
+
+instance = new NotificationService();
+module.exports = instance;
