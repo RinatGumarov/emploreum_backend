f20db325bbdac95c259c2688f57cbf135494670c
rinat <tiran678@icloud.com>
2018-03-01 22:35:11 +0300

    localhost log added

--- src/core/app.js
+++ src/core/app.js
@@ -19,6 +19,7 @@ class Application {
         this.express = express;
         this.server = this.express();
         this.port = process.env.PORT || this.config.port;
+        this.host = process.env.HOST || this.config.host;
         this.middlewaresIniter = new MiddlewaresIniter(this.server, this.express);
         // добавление на все роуты фильторв для корректировки запросов
         this.middlewaresIniter.correctRequest();
