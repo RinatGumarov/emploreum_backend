f2d9d98d0457c367387cffc62dabb362e2ba1a85
mikl778890@yandex.ru <adminadmin123>
2018-03-28 22:11:02 +0300

    hot fix

--- src/modules/specialisation/queries/profile-skills.sql
+++ src/modules/specialisation/queries/profile-skills.sql
@@ -2,7 +2,7 @@ SELECT
   profile_skills.id,
   profile_skills.profile_id as "profileId",
   profile_skills.skill_id as "skillId",
-  CONCAT(s2.name, '(', p.name, ')')
+  CONCAT(s2.name, '(', p.name, ')') as "name"
 FROM profile_skills
   LEFT JOIN profiles p ON profile_skills.profile_id = p.id
   LEFT JOIN skills s2 ON profile_skills.skill_id = s2.id
