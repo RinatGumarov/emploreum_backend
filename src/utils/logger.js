9fac3493a69acfab26c92ebafa4386ca6150c674
mikl778890@yandex.ru <adminadmin123>
2018-04-14 19:29:11 +0300

    перенаправления потока выводов

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -7,38 +7,55 @@ const config = require('../utils/config');
 let loggerInstance;
 
 class Logger {
-
-    constructor() {
-        let logsDir = process.env.LOG_FILE || config.get("file").log_file;
-        let errorDir = process.env.LOG_ERROR_FILE || config.get("file").log_error_file;
-
-        let access = fs.createWriteStream(logsDir, {flags: 'a'});
-        let error = fs.createWriteStream(errorDir, {flags: 'a'});
-
-        // redirect stdout / stderr
+    
+    /**
+     * создать и установить файлы для вывода логов
+     */
+    createLogsFile() {
+        let normalLogFileName = this.createSingleLogFile("normal");
+        let errorLogFileName = this.createSingleLogFile("error");
+        let access = fs.createWriteStream(normalLogFileName, {flags: 'a'});
+        let error = fs.createWriteStream(errorLogFileName, {flags: 'a'});
         process.stdout.pipe(access);
         process.stderr.pipe(error);
-
     }
-
+    
+    
+    /**
+     * создать файл лога если его нету
+     * @param logType
+     * @returns {string}
+     */
+    createSingleLogFile(logType) {
+        let filename = `${this.logsDir}/${logType}-${new Date().getTime()}.txt`;
+        if (!fs.existsSync(filename)) {
+            fs.appendFile(filename);
+        }
+        
+        return filename;
+    }
+    
+    
+    constructor() {
+        this.logsDir = process.env.LOG_DIR || config.get("file").log_dir;
+        this.createLogsFile();
+        setInterval(this.createLogsFile, 6000 * 60 * 24)
+    }
+    
     /**
      * @param err
      * метод для логирования ошибок
      */
     error(err) {
-        if (process.env.NODE_ENV !== "test") {
-            console.error(chalk.red(err));
-        }
+        console.error(chalk.red(err));
     }
-
+    
     /**
      * @param msg
      * метод для записи логов не связанных с ошибками
      */
     log(msg) {
-        if (process.env.NODE_ENV !== "test") {
-            console.log(msg)
-        }
+        console.log(msg)
     }
 }
 
